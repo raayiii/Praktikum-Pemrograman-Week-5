b31172b96b470422193a79649809a7b6b1fc3bf7
raayiii <raayiii18@gmail.com>
2025-10-08 10:59:39 +0700

    Update tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -9,6 +9,26 @@ s = s + arr[i];
 return s;
 }
 
+int minVal(const int a[], int n) {
+    int minv = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] < minv)
+            minv = a[i];
+    return minv;
+}
+
+int maxVal(const int a[], int n) {
+    int maxv = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] > maxv)
+            maxv = a[i];
+    return maxv;
+}
+
+double mean(const int a[], int n) {
+    int total = jumlah(a, n);
+    return (double)total / n;
+}
 
 int main (){
 	
@@ -24,6 +44,9 @@ int main (){
 }
 	s = jumlah(arr, n);
 	cout << "Jumlah semua elemen: " << s;
+    cout << "\nNilai minimum: " << minVal(arr, n);
+    cout << "\nNilai maksimum: " << maxVal(arr, n);
+    cout << "\nRata-rata: " << mean(arr, n) << endl;
 	
 	return 0;
 }
